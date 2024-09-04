b05a1bada2992f08adc35db3b2f2af8c0af54968
CryptoMorki3s <cryptomorki3s@gmail.com>
2024-09-04 21:11:45 +0100

    first commit

--- client/landoftheforgotten/src/main.tsx
+++ client/landoftheforgotten/src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './index.js'
-const manifestUrl = `https://github.com/OiseEjemai/tonconnect-manifest-json/blob/main/tonconnect-manifest.json`
+const manifestUrl = `https://raw.githubusercontent.com/OiseEjemai/tonconnect-manifest-json/main/tonconnect-manifest.json`
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
